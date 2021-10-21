7c197e128b393718b39bf6bd7343a8988860b615
Kim Novak <kimnovak187@gmail.com>
2021-10-21 02:10:01 +0200

    Render initial data

--- js/search.js
+++ js/search.js
@@ -1,5 +1,31 @@
+const data = [{title: 'fe'}, {title: 'devops'}];
+
 window.addEventListener('load', init);
 
+
 function init() {
-    alert('init')
+    const $jobsList = document.getElementById('jobs-list');
+    renderJobs({$listContainer: $jobsList, data});
+}
+
+function renderJobs({$listContainer, data}) {
+    const $container = new DocumentFragment();
+
+    if (!Array.isArray(data) || !data.length) {
+        const $noResults = document.createElement('li');
+        $noResults.innerHTML = 'No results';
+        $listContainer.append($noResults);
+
+        return;
+    }
+
+    data.forEach(job => {
+        const $job = document.createElement('li');
+        $job.classList.add('js-job');
+        $job.textContent = job.title;
+
+        $container.append($job);
+    });
+
+    $listContainer.append($container);
 }
